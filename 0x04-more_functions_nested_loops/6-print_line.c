ccdc795a1eabb312dad45749cd6c74dc2249c604
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-02-23 15:30:30 -0800

    no.6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_line - it prints a line
+*@n: the length of the line
+*Return: void
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
