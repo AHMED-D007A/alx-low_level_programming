eaffb5429c57fb75dcb23b139c045cf629fdf519
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-02-23 08:45:51 -0800

    no.5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_sign - print the sign of any number
+*@c: the number used for checking
+*Return: 1 if positive
+*Return: 0 if zero
+*Return: -1 if negative
+*/
+int print_sign(int c)
+{
+	int r = (n > 0) ? 1 : (n < 0) ? -1 : 0;
+
+	if (r == 1)
+	{
+		_putchar('+');
+	}
+	else if (r == -1)
+	{
+	 	_putchar('-');
+	}
+	else
+	{
+		_putchar('0');
+	}
+
+	return (r);
+}
