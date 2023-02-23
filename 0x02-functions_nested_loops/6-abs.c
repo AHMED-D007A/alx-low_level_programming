cdbec92b779c99e4b8e6c30a0e44c93601bc5ae2
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-02-23 08:55:23 -0800

    no.6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_abs - print the abslote value of any int
+*@n: the number for checking
+*Return: n
+*/
+int _abs(int n)
+{
+	return (n <= 0 ? n * -1 : n);
+}
