dd361b60700dfc849384355914aab39df6004d0a
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-02-23 17:09:15 -0800

    no.9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,15 +10,13 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
-			printf("Fizz ");
-		if (i % 5 == 0 && i != 100)
-			printf("Buzz ");
-		if (i == 100)
+			printf("Fizz");
+		if (i % 5 == 0)
 			printf("Buzz");
 		else if ((i % 3 != 0) && (i % 5 != 0))
-			printf("%d ", i);
+			printf("%d", i);
 	}
-	printf("\n");
+		printf("%c", i < 100 ? ' ' : '\n');
 
 	return (0);
 }
