6f39792aa2f872158f4b36e156f4015d81b49a80
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-02-27 13:00:03 -0800

    no.4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_rev - prints a string, followed by a new line, to stdout.
+*@s: pointer
+*/
+void print_rev(char *s)
+{
+	char c = *s;
+
+	while (c != '\0')
+		c = *++s;
+	while (c != *str)
+	{
+		c = *--s;
+		_putchar(c);
+	}
+	c = *s;
+	_putchar(c);
+	_putchar('\n');
+}
