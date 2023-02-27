0d887fd9e2bbabab07bd4ededa916dff94c81c84
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-02-27 13:05:30 -0800

    no.4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,18 @@
 #include "main.h"
 /**
-*print_rev - prints a string, followed by a new line, to stdout.
-*@s: pointer
-*/
+ * print_rev - prints a string in reverse, followed by a newline
+ * @s: the reference to the string
+ */
 void print_rev(char *s)
 {
+	int h = 0;
 	char c = *s;
 
 	while (c != '\0')
-		c = *++s;
-	while (c != *s)
-	{
-		c = *--s;
-		_putchar(c);
-	}
-	c = *s;
-	_putchar(c);
+		c = *(s + ++h);
+
+	c = (*s + --h);
+	while (i >= 0)
+		_putchar(*(s + h--));
 	_putchar('\n');
 }
