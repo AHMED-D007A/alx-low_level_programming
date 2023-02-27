94d73c68ea03c343c02b5fa80600e6f8fd16e0a7
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-02-27 12:27:40 -0800

    no.2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*strlen - returns the length of a string.
+*@s: the pointer
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+	int arr[] = *s;
+
+	for (arr[i] != '\0'; i++)
+
+	return (i);
+}
