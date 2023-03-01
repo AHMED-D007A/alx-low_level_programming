986d2416b149c3e765a59768972a1e71ae242e3b
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-03-01 10:14:32 -0800

    no.0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,27 @@
 #include "main.h"
 /**
- _stract - concatenates two strings.
- @dest: first string
- @src: second string
- Return: dest
+* _strcat - concatenates two strings.
+* @dest: first string
+* @src: second string
+* Return: dest
 */
 char *_strcat(char *dest, char *src)
 {
 	int i, c;
 
 	for (i = 0; dest[i] != '\0'; i++)
-		;
+	{
+	}
+
+	dest[i] = ' ';
 
 	for (c = 0; src[c] != '\0'; c++)
 	{
-		dest[i] = src[c];
 		i++;
+		dest[i] = src[c];
 	}
+
+	i++;
 	dest[i] = '\0';
 
 	return (dest);
