8d317718cd2201e74508edc540cc6a1e3074b069
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-03-02 13:25:18 -0800

    no.6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,14 +14,18 @@ char *cap_string(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] == '\n' || str[i] == '\t' || str[i] == '.' || str[i] == ' ')
-			if (str[i + 1] > 96 && str[i + 1] < 123)	
+			if (str[i + 1] > 96 && str[i + 1] < 123)
 				str[i + 1] -= 32;
 
 		if (str[i] == '?' || str[i] == '!' || str[i] == ';' || str[i] == ',')
 			if (str[i + 1] > 96 && str[i + 1] < 123)
 				str[i + 1] -= 32;
 
-		if (str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{'|| str[i] == '}')
+		if (str[i] == '"' || str[i] == '(' || str[i] == ')')
+			if (str[i + 1] > 96 && str[i + 1] < 123)
+				str[i + 1] -= 32;
+
+		if (str[i] == '{' || str[i] == '}')
 			if (str[i + 1] > 96 && str[i + 1] < 123)
 				str[i + 1] -= 32;
 	}
