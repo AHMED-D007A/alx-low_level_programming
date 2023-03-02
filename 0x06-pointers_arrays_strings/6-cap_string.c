26133900134e7058042b910d39f59df3c8707748
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-03-02 13:12:49 -0800

    no.6 :

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*cap_string - capitalizes all words of a string.
+*@str: the string
+*Return: the string
+*/
+char *cap_string(char *str)
+{
+	if (str[0] > 96 && str[0] < 123)
+		str[0] -= 32;
+
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == '\n' || str[i] == '\t' || str[i] == '.' || str[i] == ' ')
+			if (str[i + 1] > 96 && str[i + 1] < 123)
+				str[i + 1] -= 32;
+	}
+
+	return (str);
+}
