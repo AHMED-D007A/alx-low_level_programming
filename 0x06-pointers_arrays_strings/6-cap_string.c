9009b134b89c6744e9133309310b00a67ba1e4e5
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-03-02 13:36:59 -0800

    no.6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,15 +17,15 @@ char *cap_string(char *str)
 			if (str[i + 1] > 96 && str[i + 1] < 123)
 				str[i + 1] -= 32;
 
-		if (str[i] == '?' || str[i] == '!' || str[i] == ';' || str[i] == ',')
+		else if (str[i] == '?' || str[i] == '!' || str[i] == ';' || str[i] == ',')
 			if (str[i + 1] > 96 && str[i + 1] < 123)
 				str[i + 1] -= 32;
 
-		if (str[i] == '"' || str[i] == '(' || str[i] == ')')
+		else if (str[i] == '"' || str[i] == '(' || str[i] == ')')
 			if (str[i + 1] > 96 && str[i + 1] < 123)
 				str[i + 1] -= 32;
 
-		if (str[i] == '{' || str[i] == '}')
+		else if (str[i] == '{' || str[i] == '}')
 			if (str[i + 1] > 96 && str[i + 1] < 123)
 				str[i + 1] -= 32;
 	}
