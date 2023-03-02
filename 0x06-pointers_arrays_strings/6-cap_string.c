0b6654eddd0534983db0691409c5d7cfca565b78
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-03-02 14:19:53 -0800

    no.6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,37 +1,39 @@
 #include "main.h"
+
 /**
-*cap_string - capitalizes all words of a string.
-*@str: the string
-*Return: the string
-*/
+ * cap_string - capitalize all words of a string
+ * @str: string
+ * Return: `str`
+ */
+
 char *cap_string(char *str)
 {
-	if (str[0] > 96 && str[0] < 123)
-		str[0] -= 32;
+	int i, c;
+	int trigger;
+	char nots[] = ",;.!?(){}\n\t\" ";
 
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0, trigger = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == '\t')
-			str[i] = ' ';
+		if (str[0] > 96 && str[0] < 123)
+			trigger = 1;
+		for (c = 0; nots[c] != '\0'; c++)
+		{
+			if (nots[c] == str[i])
+				trigger = 1;
+		}
 
-		if (str[i] == '\n' || str[i] == '\t' || str[i] == '.' || str[i] == ' ')
-			if (str[i + 1] > 96 && str[i + 1] < 123)
-				str[i + 1] -= 32;
-
-		if (str[i] == '?' || str[i] == '!' || str[i] == ';' || str[i] == ',')
-			if (str[i + 1] > 96 && str[i + 1] < 123)
-				str[i + 1] -= 32;
-
-		if (str[i] == '"' || str[i] == '(' || str[i] == ')')
-			if (str[i + 1] > 96 && str[i + 1] < 123)
-				str[i + 1] -= 32;
-
-		if (str[i] == '{' || str[i] == '}')
-			if (str[i + 1] > 96 && str[i + 1] < 123)
-				str[i + 1] -= 32;
+		if (trigger)
+		{
+			if (str[i] > 96 && str[i] < 123)
+			{
+				str[i] -= 32;
+				trigger = 0;
+			}
+			else if (str[i] > 64 && str[i] < 91)
+				trigger = 0;
+			else if (str[i] > 47 && str[i] < 58)
+				trigger = 0;
+		}
 	}
-
 	return (str);
 }
