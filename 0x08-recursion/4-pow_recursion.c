6a52d6369a2e46b2dc2f7d0313c380a5eed12bf4
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-03-08 11:44:26 -0800

    no.4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,9 +15,6 @@ int _pow_recursion(int x, int y)
 		return (1);
 	if (y == 1)
 		return (x);
-	if (y > 1)
-	{
-		n = n * (_pow_recursion(x, y - 1));
-		return (n);
-	}
+
+	n = n * (_pow_recursion(x, y - 1));
 }
