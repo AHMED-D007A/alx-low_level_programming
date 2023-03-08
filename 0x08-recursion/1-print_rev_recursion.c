edbfba1df922d299de01af447c6a5e3b6db2857d
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-03-08 10:09:52 -0800

    no.1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-*_puts_rev_recursion - prints a string, followed by a new line
+*_print_rev_recursion - prints a string, followed by a new line
 *@s: the pointer that point to the string
 */
-void _puts_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 		return;
 	else
 	{
-		_puts_rev_recursion(s++);
+		_print_rev_recursion(++s);
 		_putchar(*s);
 	}
 }
