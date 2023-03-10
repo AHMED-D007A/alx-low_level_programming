f1aea007f50327575e8625b71248045b7986b9ae
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-03-10 14:42:46 -0800

    no.1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - print the number of argumens
+*@argc: the number of the argumrnts
+*@argv: the arguments
+*Return: 0
+*/
+int main(int argc, char **argv __attribute__ ((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
