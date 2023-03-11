1465c9547f6b35c210f7ec48d692ea61c5ecd008
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-03-10 16:29:16 -0800

    no.4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,36 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <ctype.h>
+
 /**
-*main - main block
-*@argc: number of arguments
-*@argv: grgument
-*Return: 0
-*/
+ * isdigits - check if string is a digit characters only
+ * @s: the reference to a character
+ * Return: 1 if @s reference to cstring otherwise returns 0
+ */
+int isdigits(char *s)
+{
+	for (; *s; s++)
+		if (!isdigit(*s))
+			return (0);
+	return (1);
+}
+
+/**
+ * main - prints the sum of all arguments
+ * @argc: the number or arguments
+ * @argv: pointer of pointer of char
+ * Return: 0
+ */
 int main(int argc, char **argv)
 {
-	int i, sum = 0;
+	int c, l, sum = 0;
 
-	if (argc < 2)
+	for (l = 1; l < argc; l++)
 	{
-		printf("0\n");
-		return (0);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		if (atoi(argv[i]) < 0)
-			return (0);
-		else if (isdigit(atoi(argv[i])))
-		{
-			sum += atoi(argv[i]);
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-	}
-	printf("%d", sum);
+		char *arg = *(argv + l);
 
+		if (!isdigits(arg))
+		{
+			puts("Error");
+			exit(1);
+		}
+
+		c = atoi(arg);
+		sum += c;
+	}
+
+	printf("%d\n", sum);
 	return (0);
 }
