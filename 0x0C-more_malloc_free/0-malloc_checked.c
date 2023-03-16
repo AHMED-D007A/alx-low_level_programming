d6c712ff75e943ac12380d8252a04247b591ff80
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-03-16 16:16:10 -0700

    no.0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,6 +2,7 @@
 /**
 *malloc_checked - a function that allocates memory using malloc.
 *@b: the size of the memory
+*Return: return the pointer that point to the heap
 */
 void *malloc_checked(unsigned int b)
 {
