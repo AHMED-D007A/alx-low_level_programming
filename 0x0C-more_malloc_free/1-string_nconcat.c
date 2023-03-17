392369ca4617b50c9d70b1da8968eb45d25f671d
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-03-17 08:56:56 -0700

    no.1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,25 +23,26 @@ unsigned int _strlen(char *s)
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	unsigned int l, i, strlen;
-
-	if (ptr == NULL)
-		return (NULL);
+	char *p;
+	int strlen1, i, c;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	strlen = _strlen(s1);
-	ptr = malloc((n + strlen + 1) * sizeof(char));
+	strlen1 = (unsigned int)_strlen(s1);
+	p = malloc((strlen1 + n + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0, c = 0; i < (strlen1 + n); i++)
+	{
+		if (i < strlen1)
+			p[i] = s1[i];
+		else
+			p[i] = s2[c++];
+	}
+	p[i] = '\0';
 
-	for (i = 0; s[i] != '\0'; i++)
-		ptr[i] = s1[i];
-	for (l = 0; l < n; l++)
-		ptr[i++] = s2[l];
-	ptr[i] = '\0';
-
-	return (ptr);
+	return (p);
 }
