a402a5be61d0344da1501ca0879188bac2d5c7bd
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-03-22 16:18:17 -0700

    fixing no.0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,8 +5,9 @@
  * print_name - prints a name
  * @name: the string that will be printed
  * @f: function pointer that takes a string statment
+ * Return: nothing
 */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	(*f)(name);
 }
