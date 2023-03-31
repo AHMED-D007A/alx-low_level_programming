c8828d6c55af4e99f3e501eed79af8cbd7216913
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-03-30 22:03:25 -0700

    Fixing

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 	{
 		printf("[%d] %s\n",
 				h->str ? h->len : 0,
-				h->str ? h->str : "(nill)");
+				h->str ? h->str : "(nil)");
 		h = h->next;
 		cownt++;
 	}
