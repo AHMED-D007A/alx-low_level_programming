06f901090bf90b0c38e1d0a477e590a00e73ee39
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-03-30 21:56:00 -0700

    No.1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
 */
 size_t print_list(const list_t *h)
 {
-	int cownt;
+	int cownt = 0;
 
 	while (h)
 	{
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*list_len - a function that returns the number of elements in a linked list_t list.
+*@h: a pointer to the list
+*Return: the number of element in the function
+*/
+size_t list_len(const list_t *h)
+{
+	int cownter = 0;
+
+	while (h)
+	{
+		cownter++;
+		h = h->next;
+	}
+
+	return (cownter);
+}
