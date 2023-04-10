c51370fe9a633160d6091e93d6a40c0e43ac4c61
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-04-10 09:01:51 -0700

    fixing

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,17 +12,20 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC | S_IWUSR, 0600);
 
-	if (fd == -1)
+	if (fd < 0)
+	{
+		close(fd);
 		return (-1);
+	}
 
 	if (text_content)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
 		{
 			con1 = write(fd, text_content, i);
-			if (con1 == -1)
+			if (con1 < 0)
 				return (-1);
 		}
 	}
