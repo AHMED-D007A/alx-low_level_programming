45daa94ae0bd103112aa0764b44a1d08cd26046a
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-04-10 09:57:26 -0700

    No.1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,12 +28,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-			con1 = write(fd, text_content, strlen(text_content));
-			if (con1 < 0)
-			{
-				close(fd);
-				return (-1);
-			}
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+		con1 = write(fd, text_content, i);
+		if (con1 < 0)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
