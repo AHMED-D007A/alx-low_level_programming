be90796e5bd6d0d8e275b17c6f11b888488e51c2
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-04-10 10:53:02 -0700

    No.3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,24 +7,30 @@
 */
 int main(int argc, char *argv[])
 {
-		int input_fd, output_fd, istatus, ostatus;
+	int input_fd, output_fd, istatus, ostatus;
 	char buf[1024];
 	mode_t mode;
 
 	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	if (ac != 3)
+	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	input_fd = open(av[1], O_RDONLY);
+	input_fd = open(argv[1], O_RDONLY);
 	if (input_fd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	output_fd = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	output_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
 	if (output_fd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 	do {
 		istatus = read(input_fd, buf, 1024);
 		if (istatus == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 		if (istatus > 0)
@@ -32,17 +38,23 @@ int main(int argc, char *argv[])
 			ostatus = write(output_fd, buf, (ssize_t) istatus);
 			if (ostatus == -1)
 			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 				exit(99);
 			}
 		}
 	} while (istatus > 0);
 	istatus = close(input_fd);
 	if (istatus == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", input_fd), exit(100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", input_fd);
+		 exit(100);
+	}
 	ostatus = close(output_fd);
 	if (ostatus == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", output_fd), exit(100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", output_fd);
+		exit(100);
+	}
 	return (0);
 }
 
