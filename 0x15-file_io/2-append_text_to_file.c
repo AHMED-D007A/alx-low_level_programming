8dc3c8fca4fe5f75aabe568fcc22b13227cc2dae
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-04-10 10:18:31 -0700

    fixing

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,9 +12,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	if (!text_content)
-		return (-1);
-
 	fd = open(filename, O_APPEND | O_RDWR);
 
 	if (fd < 0)
@@ -22,6 +19,9 @@ int append_text_to_file(const char *filename, char *text_content)
 		close(fd);
 		return (-1);
 	}
+
+	if (!text_content)
+		return (-1);
 
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
