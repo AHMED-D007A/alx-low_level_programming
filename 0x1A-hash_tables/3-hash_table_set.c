ea0ec6e0023e0e61d96c825fa0f04ff3d142834b
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-06-24 08:13:37 +0300

    task no.3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - a function that adds an element to the hash table.
+ * @ht: the given table.
+ * @key: the given key to add or to update it.
+ * @value: the value of the given key.
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int indx;
+	hash_node_t *nodde = NULL;
+
+	if (!ht || !key || !value || strlen(key) == 0)
+		return (0);
+
+	indx = key_index((const unsigned char *)key, ht->size);
+	nodde = ht->array[indx];
+
+	while (nodde)
+	{
+		if (strcmp(key, nodde->key))
+		{
+			nodde->value = (char *)value;
+			return (1);
+		}
+		else
+		{
+			nodde = nodde->next;
+		}
+		nodde->key = (char *)key;
+		nodde->value = (char *)value;
+	}
+
+		return (1);
+}
