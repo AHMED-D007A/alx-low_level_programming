221b713195d5f7dcc60a4584b8222ad55cd6fd37
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-07-26 08:26:56 +0300

    task no.5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,4 +4,18 @@
 
 def island_perimeter(grid):
     """a function that returns the perimeter of the island described in grid"""
-    
+    height = 0
+    width = 0
+    measure = 0
+    for list in grid:
+        measure = 0
+        for cells in list:
+            if cells == 1:
+                height += 1
+                break
+        for cell in list:
+            if cell == 1:
+                measure += 1
+        if measure > width:
+            width = measure
+    return (height + width) * 2
