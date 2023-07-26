c24c0102c587a55d78ec3f78e6cdeede4bc236d9
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-07-26 07:59:37 +0300

    testing

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,7 @@
+#/usr/bin/python3
+"""return the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """a function that returns the perimeter of the island described in grid"""
+    
