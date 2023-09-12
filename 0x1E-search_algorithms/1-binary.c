730c8be5802b271e4f94b10786737b1251fc95ed
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-09-12 07:51:31 +0300

    debugging

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -16,10 +16,11 @@ int _binary_search(int *array, int value, size_t first, size_t last)
 	for (i = first + 1; i <= last; i++)
 		printf(", %d", array[i]);
 	putchar('\n');
-	if (first > last)
-		return (-1);
+
 	if (array[mid] == value)
 		return (mid);
+	else if (first > last)
+                return (-1);
 	else if (array[mid] > value)
 		return (_binary_search(array, value, first, mid - 1));
 	else
